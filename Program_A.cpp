c5006d5e438494d945f57a083f103f5984586fa6
Md. Rabiul Islam (Tuhin) <mrit.cse.ru@gmail.com>
2021-01-29 22:21:40 +0600

    Update Program_A.cpp
    Fixing calculation bug.

--- Program_A.cpp
+++ Program_A.cpp
@@ -20,22 +20,7 @@ int main()
     cout << "****** Welcome to User A's Window ******\n\n\n";
     while (true) {
 
-        int primeNumber, primitiveRoot, secretKey, publicA, publicB, symmetricKey;
-
-        FILE* inp = fopen("server.txt", "rb");
-
-        fscanf(inp, "%d %d", &primeNumber, &primitiveRoot);
-
-        secretKey = rand() % primeNumber;
-        publicA = pow(primitiveRoot, secretKey, primeNumber);
-        cout << "A's public key is: " << publicA << endl << "Please input B's public key: ";
-        cin >> publicB;
-        symmetricKey = pow(primitiveRoot, publicB, primeNumber);
-        cout << "Computed symmetric key: " << symmetricKey << endl << endl;
-        fclose(inp);
-
         cout<<"\nPress 1, to continue.\nPress 2, to exit.\n";
-        bac:;
         int x;
         cin>>x;
         cout<<endl;
@@ -43,8 +28,22 @@ int main()
             break;
         if (x != 1) {
             cout<<"Wrong press. Please press 1 or 2.\n";
-            goto bac;
+            continue;
         }
+
+        int primeNumber, primitiveRoot, secretKey, publicA, publicB, symmetricKey;
+
+        FILE* inp = fopen("server.txt", "rb");
+
+        fscanf(inp, "%d %d", &primeNumber, &primitiveRoot);
+
+        secretKey = (rand() + time(NULL)) % primeNumber;
+        publicA = pow(primitiveRoot, secretKey, primeNumber);
+        cout << "A's public key is: " << publicA << endl << "Please input B's public key: ";
+        cin >> publicB;
+        symmetricKey = pow(publicB, secretKey, primeNumber);
+        cout << "Computed symmetric key: " << symmetricKey << endl << endl;
+        fclose(inp);
     }
     return 0;
 }
